9c127ae2b790d87177a0b9262a5db02422c9f0fa
ren0nmansoneves@gmail.com <ren0nmansoneves@gmail.com>
2025-07-02 21:49:36 -0300

    atualizado

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import styles from "./Card.module.css";
+
+export const Card = ({ name, description, image, onDelete }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
+      <div className={styles.content}>
+        <h3 className={styles.name}>{name}</h3>
+        <p className={styles.description}>{description}</p>
+        <button className={styles.delete} onClick={onDelete}>
+          🗑️
+        </button>
+      </div>
+    </div>
+  );
+};
